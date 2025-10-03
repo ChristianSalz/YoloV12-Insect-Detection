1a9fb951f9805e25661f6d4a2393612aa2d83c5c
ChristianSalz <49674749+ChristianSalz@users.noreply.github.com>
2025-10-03 12:06:46 +0200

    Added parameter for multi CPU usage , but still limited results (clsoses #12)

--- trainTheModelV12.py
+++ trainTheModelV12.py
@@ -1,21 +1,18 @@
 from ultralytics import YOLO
 import torch
 import os
-torch.set_num_threads(os.cpu_count())
 
 # Check if MPS is available
 # device = 'mps' if torch.backends.mps.is_available() else 'cpu'
 # print(f"Training auf Gerät: {device}")
 
 # Define model and dataset paths
-model_path = "yolo12n.pt"  # Path to your model configuration
+model_path = "yolo12m.pt"  # Path to your model configuration
 data_path = 'data.yaml'  # Path to your dataset YAML file
 
 # Load the YOLO model
 model = YOLO(model_path)
 
 # Train the model
-results = model.train(data=data_path, epochs=100, imgsz=640)  # Train the model
-
-#pip install ultralytics
-#pip install torch torchvision torchaudio
+# workers= to the amount of CPU cores of your machine, you can aso experiment with the batch=X parameter
+results = model.train(data=data_path, epochs=125, imgsz=640, workers=8)  # Train the model
